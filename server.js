a7c37df70b9924cf036cae1a299b96f79e0993cf
beasleyDotcom <beasley.com@gmail.com>
2020-07-13 16:18:15 -0700

    ???

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get('/location', (request, response) => {
 
 });
 
+
+
+
+console.log("what do you think about that? HUH?")
+app.get('*', (request, response) => {
+    response.status(404).send('HAHA SUCKER! PAGE NOT FOUND');
+});
+
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 })
