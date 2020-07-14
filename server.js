a6cbfb6d26188baffd8b802fad68ae5fa3fd8d0b
beasleyDotcom <beasley.com@gmail.com>
2020-07-13 17:25:25 -0700

    maybe???

--- server.js
+++ server.js
@@ -4,13 +4,27 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+
+//this bouncer would let a highschooler into the bar
 app.use(cors());
 const PORT = process.env.PORT || 3001;
 app.get('/location', (request, response) => {
+        let longLats = require('data/location.json');
+        let cityName = ((longLats) => {
+            let city = longLats[0].display_name.split(',');
+            console.log("this is your city", city[0]);
+            
+        });
+        const obj = new Location(longLats);
 
+// this is the end of the try.        
 });
 
-
+function Location (location){
+    this.search_query = 
+    this.longitude =
+    this.latitude =
+};
 
 
 console.log("what do you think about that? HUH?")
