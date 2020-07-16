0dfa782b6e4aeffb928bc9fd8c1ffdbf87a348e9
beasleyDotcom <beasley.com@gmail.com>
2020-07-15 21:31:07 -0700

    by george I think I've got it! (thanks to david, chance, and megan)

--- schema.sql
+++ schema.sql
@@ -1,12 +1,8 @@
-DROP TABLE IF EXISTS locations;
-
-CREATE TABLE locations (
-    id SERIAL PRIMARY KEY,
-    search_query VARCHAR(255),
-    formatted_query VARCHAR(255),
-    latitude NUMERIC(10,7),
-    longitude NUMERIC(10,7)
+CREATE TABLE IF NOT EXISTS 
+locations(
+    id SERIAL PRIMARY KEY NOT NULL,
+    search_query VARCHAR(255) NOT NULL,
+    formatted_query VARCHAR(255) NOT NULL,
+    latitude NUMERIC(10,7) NOT NULL,
+    longitude NUMERIC(10,7) NOT NULL
 );
-
-INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ('Tia', 'Low', '23.0000909', '45.555554443');
-SELECT * FROM locations;
