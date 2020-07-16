818166ea5b8fcb7b9d438f17fd08e7bb35896ed7
beasleyDotcom <beasley.com@gmail.com>
2020-07-15 20:43:12 -0700

    maybe??

--- schema.sql
+++ schema.sql
@@ -4,9 +4,9 @@ CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
     search_query VARCHAR(255),
     formatted_query VARCHAR(255),
-    latitude VARCHAR(255),
-    longitude VARCHAR(255)
+    latitude NUMERIC(10,7),
+    longitude NUMERIC(10,7)
 );
 
-INSERT INTO locations (search_query, formatted_query) VALUES ('Tia', 'Low');
+INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ('Tia', 'Low', '23.0000909', '45.555554443');
 SELECT * FROM locations;
--- server.js
+++ server.js
@@ -6,46 +6,76 @@ const cors = require('cors');
 const superagent = require('superagent');
 require('dotenv').config();
 const pg = require('pg');
-
 //this bouncer (cors) would let a highschooler into the bar
 app.use(cors());
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.WINDOWS_DATABASE_URL);
 client.on('error', err => {
-    console.log('er ROAR!!!', err);
+    console.log('er ROAR!!! on line 13', err);
 });
 
 const PORT = process.env.PORT || 3001;
 //if you need to close all of your ports. In your termnial you 'pkill node'
 //let's get the data from the location api
-
-
-
-
-
 app.get('/location', bungleLocation);
+app.get('/weather', bungleWeather);
+app.get('/trails', bungleTrails);
+// what does the database return if it doesn't have what I'm looking for?
+//what does the API return if it doesn't have what I'm looking for?
+//create if/else, if both return NaN or whatever return ''?
+function addLocation(object){
+    // add city name, formatted city name, latitude, and longitude.
+    console.log('search query (city):', object.query.search_query)
+    console.log('latitude here:', object.query.latitude)
+    let search_query = object.search_query;
+    let formatted_query = object.formatted_query;
+    let latitude = object.latitude;
+    let longitude = object.longitude;
+    let sqlStatement = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4, $5);';
+    let safeValues = [search_query, formatted_query, latitude, longitude];
+    client.query(sqlStatement, safeValues);
+}
+// function getLocations(request, response){
+//     let sql = 'SELECT search_query FROM locations;';
+//     client.query(sql)
+//     .then(resultsFromPostgres => {
+//         let city = resultsFromPostgres.rows;
+//         response.send(city);
+//     }).catch( err => console.log(err));
+// }
+
 function bungleLocation(request, response){    
     let city = request.query.city;
-    let url = 'https://us1.locationiq.com/v1/search.php';
-    let mapParams = {
-        key: process.env.GEOCODE_API_KEY,
-        q: city,
-        format: 'json',
-        limit: 1
-    };
-    
-    superagent.get(url)
-        .query(mapParams)
-        .then(resultFromSuperagent => {
-            let mapData = resultFromSuperagent.body;
-            const obj = new Location(city, mapData);
-            response.status(200).send(obj);
-        }).catch((error) => {
-            console.log("error with superagent", error);
-            response.status(500).send("we messed OOPS orry");
-        });
-}
+    let safeValues = [city];
+    let searchString = 'SELECT * FROM locations WHERE search_query=$1;';
+    client.query(searchString, safeValues)
+    .then(location =>{
+        if(location.rowCount > 0){
+            response.send(location.rows[0]);
+        }
+        else{
+            let url = 'https://us1.locationiq.com/v1/search.php';
+            let mapParams = {
+                key: process.env.GEOCODE_API_KEY,
+                q: city,
+                format: 'json',
+                limit: 1
+            };    
+            superagent.get(url)
+            .query(mapParams)
+            .then(resultFromSuperagent => {
+                let mapData = resultFromSuperagent.body;
+                const obj = new Location(city, mapData);
+                addLocation(obj);
+                response.status(200).send(obj);
+            }).catch((error) => {
+                console.log("error with superagent", error);
+                response.status(500).send("we messed OOPS orry");
+            });
 
-app.get('/weather', bungleWeather);
+        }
+        })
+    };
+
 function bungleWeather(request, response){    
     let city = request.query.city;
     let url = 'https://api.weatherbit.io/v2.0/forecast/daily';
@@ -70,7 +100,7 @@ function bungleWeather(request, response){
         });
 }
 // hiking section:
-app.get('/trails', bungleTrails);
+
 function bungleTrails(request, response){    
     let url = 'https://www.hikingproject.com/data/get-trails';
     let trailParams = {
@@ -120,7 +150,8 @@ function Trail (obj){
 app.get('*', (request, response) => {
     response.status(404).send('HAHA SUCKER! you are cool but.... PAGE NOT FOUND');
 });
-
+client.connect()
+.then(() => {
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
-})
+})})
